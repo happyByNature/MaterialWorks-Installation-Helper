29e7b618ac1ecef516762a1c5d0a709e3c2757b0
nada0 <nada001@tuta.io>
2026-03-07 15:59:06 +0200

    Add mwInstallationHelper.py with a basic "Hello, World!" script and loop

---
+++ mwInstallationHelper.py
@@ -0,0 +1,9 @@
+#hello world app
+
+print("Hello, World!")
+
+# This is a sample Python script.
+# Loop 10 times from 1 to 10 (including) and print the current loop number
+for i in range(1, 11):
+	print(f"Loop number: {i}")
+
